4118e560d037af043281df049532ca3839164489
Kerim Alagün <150130521+kerimala@users.noreply.github.com>
2025-07-07 12:24:18 +0200

    clean up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Services\AgreementService;
-use App\Services\DummyAgreementService;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(AgreementService::class, DummyAgreementService::class);
+
     }
 
     /**
